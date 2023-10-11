e4120cad7c6a20a8efd19c1e1c32bf24841d975e
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-10-11 21:50:08 +0100

    change how class functions are detected make exception importable from top level module

--- src/arg_init/arg_init.py
+++ src/arg_init/arg_init.py
@@ -3,16 +3,13 @@ Class to process arguments, environment variables and return a set of
 processed attribute values.
 """
 
-# from __future__ import annotations
-
-from inspect import stack
+from inspect import stack, getargvalues
 from os import environ
 import logging
 
 from box import Box
 
 from .exceptions import AttributeExistsError
-from .named_args import named_arguments
 from .arg import Arg
 
 
@@ -21,8 +18,8 @@ logger = logging.getLogger(__name__)
 
 class ArgInit:
     """
-    Class to process arguments, environment variables and return a set of
-    processed attribute values.
+    Class to process arguments and environment variables and collate a set of
+    processed values.
     """
 
     ARG_PRIORITY = "arg_priority"
@@ -34,11 +31,15 @@ class ArgInit:
         env_prefix: str = "",
         priority: str = DEFAULT_PRIORITY_SYSTEM,
         use_kwargs: bool = False,
-        args: None | list = None
+        is_class: bool = False,
+        set_attrs: bool = True,  # Only applicable if is_class=True
+        args: None | list = None,
     ):
         self._env_prefix = env_prefix
         self._priority = priority
         self._use_kwargs = use_kwargs
+        self._is_class = is_class
+        self._set_attrs = set_attrs
         self._args = Box()
         self._go(args)
 
@@ -47,12 +48,19 @@ class ArgInit:
         """Return the processed arguments"""
         return self._args
 
-    def _go(self, args: None | list = None):
+    def _go(self, args: None | list):
         """
         Process args and envs, storing results in self._args
         """
-        STACK_LEVEL_OFFSET = 2  # 0=ArgInit.__init__, 1 = This function, 2=calling function
-        named_args = named_arguments(frame=stack()[STACK_LEVEL_OFFSET].frame, include_kwargs=self._use_kwargs)
+        STACK_LEVEL_OFFSET = 2  # The calling frame is 2 layers up
+        calling_stack = stack()[STACK_LEVEL_OFFSET]
+        logger.debug("InitArg called for function: %s", calling_stack.function)
+        self._process_args(calling_stack.frame, args)
+        self._set_class_attrs(calling_stack.frame)
+        logger.debug("Finished processing arguments")
+
+    def _process_args(self, frame, args):
+        named_args = self._named_arguments(frame)
         for name, value in named_args.items():
             logger.debug("Processing: %s", name)
             arg = self._find_arg(name, args)
@@ -60,18 +68,38 @@ class ArgInit:
                 self._arg_priority(arg, value)
             else:
                 self._env_priority(arg, value)
-        logger.debug("Finished processing arguments")
 
-    def set(self, obj):
-        """Set attributes as defined in "args" for the specified object."""
-        for arg, value in self.args.items():
-            if hasattr(obj, arg):
-                raise AttributeExistsError(arg)
-            setattr(obj, arg, value)
+
+    def _named_arguments(self, frame):
+        """
+        Returns a dictionary containing key value pairs of all
+        named arguments and optionally kwargs and their values,
+        associated with the specified frame, or the calling function.
+        """
+        arginfo = getargvalues(frame)
+        args = {
+            arg: arginfo.locals.get(arg)
+            for count, arg in enumerate(arginfo.args)
+            if not self._is_class_arg(count)
+        }
+        if self._use_kwargs and arginfo.keywords:
+            keywords = arginfo.keywords
+            logger.debug("Adding kwargs: %s", arginfo.locals[keywords])
+            args.update(dict(arginfo.locals[keywords].items()) if keywords else {})
+        logger.debug("Named arguments: %s", args)
+        return args
+
+
+    def _is_class_arg(self, count: int = 0) -> bool:
+        """Return True if the count is 0 and is_class is True"""
+        if self._is_class and count == 0:
+            logger.debug("Ignoring 1st argument as function is from a class")
+            return True
+        return False
+
 
     @staticmethod
     def _find_arg(name: str, args: None | list) -> str:
-        print("args=",args)
         if args:
             logger.debug("Searching for arg=%s", name)
             for arg in args:
@@ -102,7 +130,7 @@ class ArgInit:
         """
         Try and set value from arg
         """
-        logger.debug("Checking for a arg")
+        logger.debug("Checking for an arg")
         if arg.force_arg or value is not None:
             logger.debug("Arg value found: %s", value)
             self._args[self._get_attr_name(arg)] = value
@@ -115,9 +143,8 @@ class ArgInit:
         Try and set value from env
         """
         env_name = self._get_env_name(arg, self._env_prefix)
-        logger.debug("Checking env = '%s'", env_name)
-        logger.debug(arg.disable_env)
         if env_name and env_name in environ and not arg.disable_env:
+            logger.debug("Checking for env = '%s'", env_name)
             logger.debug("Found env: %s", env_name)
             value = environ[env_name]
             if arg.force_env or value:
@@ -126,7 +153,7 @@ class ArgInit:
                 return True
             logger.debug("env value is None and force_env=False. Checking default")
         else:
-            logger.debug("env not found")
+            logger.debug("env not found or checking disabled")
         return False
 
     def _try_default(self, arg: Arg) -> None:
@@ -155,3 +182,19 @@ class ArgInit:
         env = arg.env if arg.env else arg.name
         env_parts = [item for item in (prefix, env) if item]
         return "_".join(env_parts).upper()
+
+    def _set_class_attrs(self, frame):
+        """Set attributes as defined in "args" for the class object."""
+        if self._is_class and self._set_attrs:
+            logger.debug("Setting class attributes")
+            class_ref = self._get_first_arg(frame)
+            for arg, value in self.args.items():
+                if hasattr(class_ref, arg):
+                    raise AttributeExistsError(arg)
+                setattr(class_ref, arg, value)
+
+    def _get_first_arg(self, frame):
+        """Return the value of the 1st argument from the calling function."""
+        arginfo = getargvalues(frame)
+        first_arg = arginfo.args[0]
+        return arginfo.locals.get(first_arg)
