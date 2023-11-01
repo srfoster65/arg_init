da1ad70bb269fe3e6abea5d99a2e01a3f91f6aaf
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-11-01 22:26:46 +0000

    Add name argument to ArgDefaults

--- src/arg_init/_arg_defaults.py
+++ src/arg_init/_arg_defaults.py
@@ -13,13 +13,15 @@ class ArgDefaults:
     on a per argument basis.
     """
 
+    name: str
     default_value: any = None
     env_name: str = None
     disable_env: bool = False
 
     def __repr__(self) -> str:
         return (
-            f"<ArgDefaults(default_value={self.default_value}, "
+            f"<ArgDefaults(name={self.name}, "
+            f"default_value={self.default_value}, "
             f"env_name={self.env_name}, "
             f"disable_env={self.disable_env})>"
         )
