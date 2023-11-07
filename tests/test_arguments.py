bbcfdf0e92d44ccb659d9675a73198099a45f27d
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-11-07 23:06:08 +0000

    Test logical false values initialise argumets

---
+++ tests/test_arguments.py
@@ -0,0 +1,98 @@
+"""
+Test ArgDefaults
+"""
+
+from collections import namedtuple
+import logging
+
+import pytest
+
+from arg_init import ClassArgInit, FunctionArgInit, ArgDefaults, Priority
+
+logger = logging.getLogger(__name__)
+Expected = namedtuple("Expected", "key value")
+
+
+# Common test defaults
+PRIORITY_ORDER = (Priority.ARG, Priority.CONFIG, Priority.ENV, Priority.DEFAULT)
+ENV = {"ARG1": "env1_value"}
+CONFIG = '{"test": {"arg1": "config1_value"}}'
+DEFAULTS = [ArgDefaults(name="arg1", default_value="default")]
+
+
+class TestArguments:
+    """
+    Class to test ArgInit for argument priority.
+    """
+
+    @pytest.mark.parametrize(
+        "arg_value, envs, config, defaults, expected",
+        [
+            # Priority order
+            (0, None, None, DEFAULTS, Expected("arg1", 0)),
+            ("", None, None, DEFAULTS, Expected("arg1", "")),
+            (None, {"ARG1": ""}, None, DEFAULTS, Expected("arg1", "")),
+            (None, None, '{"test": {"arg1": 0}}', DEFAULTS, Expected("arg1", 0)),
+            (None, None, '{"test": {"arg1": ""}}', DEFAULTS, Expected("arg1", "")),
+        ],
+    )
+    def test_logical_false_values(
+        self, arg_value, envs, config, defaults, expected, fs
+    ):
+        """
+        Priority Order
+        1. All defined - Arg is used
+        2. Config, env and default defined - Config is used
+        2. Env and default defined - Env is used
+        3. Default is defined - Default is used
+        4. Nothing defined - None is used
+        """
+
+        def test(arg1):  # pylint: disable=unused-argument
+            args = FunctionArgInit(
+                defaults=defaults, priority=PRIORITY_ORDER
+            ).args
+            print(args[expected.key], expected.value)
+            assert args[expected.key] == expected.value
+
+        if config:
+            fs.create_file("config.yaml", contents=config)
+        with pytest.MonkeyPatch.context() as mp:
+            if envs:
+                for env, value in envs.items():
+                    mp.setenv(env, value)
+            test(arg1=arg_value)
+
+    def test_default_value(self):
+        """
+        Test overriding default_value
+        """
+
+        class Test:
+            """Test Class"""
+
+            def __init__(self, arg1=None):  # pylint: disable=unused-argument
+                name = "arg1"
+                default_value = "arg1_default"
+                defaults = [ArgDefaults(name=name, default_value=default_value)]
+                arg_init = ClassArgInit(defaults=defaults)
+                assert arg_init.args.arg1.values.default == default_value
+
+        Test()
+
+    def test_env_name(self):
+        """
+        Test overriding env_name
+        """
+
+        class Test:
+            """Test Class"""
+
+            def __init__(self, arg1=None):  # pylint: disable=unused-argument
+                name = "arg1"
+                alt_name = "ENV1"
+                defaults = [ArgDefaults(name=name, alt_name=alt_name)]
+                arg_init = ClassArgInit(defaults=defaults)
+                assert arg_init.args.arg1.env_name == alt_name
+
+        Test()
